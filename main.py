8beed62fb350d67a0ada9fa256c9efaacc2f7b08
Rohit Patil <rohit4change@yahoo.in>
2020-08-18 16:28:47 -0400

    Load Image Bug fixed

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.slider import Slider
 from kivy.uix.popup import Popup
 from kivy.graphics import Rectangle, Line, Color
-from kivy.properties import ListProperty, ObjectProperty
+from kivy.properties import ListProperty, ObjectProperty, StringProperty
 from kivy.factory import Factory
 from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics.texture import Texture
@@ -20,44 +20,19 @@ import io
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
-
-
     
+
+#################################################################################
+
 class DrawingWidget(Widget):
     target_colour_rgb = ListProperty([0, 0, 0])
 
+    def update(self, texture, width, height):
+        with self.canvas:
+            Rectangle(texture=texture, pos=self.pos, size=(width, height))
+
     def on_touch_down(self, touch):
         super(DrawingWidget, self).on_touch_down(touch)
-
-        img = Image.open('lena.png')
-        width, height = img.size
-
-        imgArr = list(img.getdata())
-        imgArr = [int(i[0]) for i in imgArr for ch in range(3)]
-        
-        arr = array('B', imgArr)
-        texture = Texture.create(size=(width, height))
-        texture.blit_buffer(arr, colorfmt='rgb',
-                            bufferfmt='ubyte')
-
-        # with self.canvas:
-        #     Rectangle(texture=texture, pos=self.pos, size=(width, height))
-
-    # def on_touch_down(self, touch):
-    #     super(DrawingWidget, self).on_touch_down(touch)
-
-    #     if not self.collide_point(*touch.pos):
-    #         return
-
-    #     with self.canvas:
-    #         Color(*self.target_colour_rgb)
-    #         self.line = Line(points=[touch.pos[0], touch.pos[1]], width=2)
-
-    # def on_touch_move(self, touch):
-    #     if not self.collide_point(*touch.pos):
-    #         return
-
-    #     self.line.points = self.line.points + [touch.pos[0], touch.pos[1]]
 
 class Interface(BoxLayout):
     pass
@@ -65,6 +40,7 @@ class Interface(BoxLayout):
 
 class FelicityApp(App):
     loadfile = ObjectProperty(None)
+    defaultPath = StringProperty('.')
     
     def build(self):
         root_widget = Interface()
@@ -74,10 +50,9 @@ class FelicityApp(App):
         self._popup = Popup(title='Load', content=content,
                       size_hint=(0.9, 0.9))
         self._popup.open()
-
+        
     def load(self, path, filename):
-        print(path)
-        print(filename)
+        print(filename[0])
         img = Image.open(filename[0])
         width, height = img.size
 
@@ -89,20 +64,7 @@ class FelicityApp(App):
         texture.blit_buffer(arr, colorfmt='rgb',
                             bufferfmt='ubyte')
 
-        # with self.canvas:
-        #     Rectangle(texture=texture, pos=self.pos, size=(width, height))
-
-        # size = 300 * 300 * 3
-        # buf = [int(x * 255 / size) for x in range(size)]
-        # # initialize the array with the buffer values
-        # arr = array('B', buf)
-        # # now blit the array
-        # texture = Texture.create(size=(300, 300))
-        # texture.blit_buffer(arr, colorfmt='rgb', bufferfmt='ubyte')
-        # now change some elements in the original array
-        self.root.ids.draw_widget.rect = Rectangle(texture=texture,
-                                                   size=(width, height))
-                
+        self.root.ids.draw_widget.update(texture, width, height)
         self.dismiss_popup()
 
     def dismiss_popup(self):
